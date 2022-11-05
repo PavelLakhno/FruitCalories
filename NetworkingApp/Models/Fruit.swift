a75b750d2fe2cb1089793aaaeaf67d24b9d67695
Pavel <lmr_16@mail.ru>
2022-11-05 15:54:25 +0300

    add parsing JSON Fruits

--- NetworkingApp/Models/Fruit.swift
+++ NetworkingApp/Models/Fruit.swift
@@ -5,4 +5,20 @@
 //  Created by Павел Лахно on 04.11.2022.
 //
 
-import Foundation
+//struct Fruits: Decodable {
+//    let fruits: [String : Fruit]?
+//}
+
+struct Fruit: Decodable {
+    let genus: String?
+    let name: String?
+    let nutritions: Nutrition?
+}
+
+struct Nutrition: Decodable {
+    let carbohydrates: Double?
+    let protein: Double?
+    let fat: Double?
+    let calories: Double?
+    let sugar: Double?
+}
--- NetworkingApp/ViewControllers/ViewController.swift
+++ NetworkingApp/ViewControllers/ViewController.swift
@@ -7,16 +7,45 @@
 
 import UIKit
 
+enum Link: String {
+    case fruitsURL = "https://www.fruityvice.com/api/fruit/all"
+    case bananaURL = "https://www.fruityvice.com/api/fruit/banana"
+}
+
+enum Alert {
+    case success
+    case failed
+    
+    var title: String {
+        switch self {
+        case .success:
+            return "Success"
+        case .failed:
+            return "Failed"
+        }
+    }
+    
+    var message: String {
+        switch self {
+        case .success:
+            return "You can see the result in the Debug area"
+        case .failed:
+            return "You can see error in the Debug area"
+        }
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        fetchFruit()
     }
     
+    // MARK: Private Methods
     private func fetchFruit() {
-        guard let url = URL(string: "https://www.fruityvice.com/api/fruit/all") else {return}
-        URLSession.shared.dataTask(with: url) { data, _, error in
+        guard let url = URL(string: Link.fruitsURL.rawValue) else {return}
+        URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
             guard let data = data else {
                 print(error?.localizedDescription ?? "No error description")
                 return
@@ -24,11 +53,26 @@ class ViewController: UIViewController {
             
             let decoder = JSONDecoder()
             do {
-                
+                let fruits = try decoder.decode([Fruit].self, from: data)
+                print(fruits)
+                self?.showAlert(withStatus: .success)
+            } catch let error {
+                self?.showAlert(withStatus: .failed)
+                print(error.localizedDescription)
             }
-        }
+        }.resume()
     }
 
-
+    private func showAlert(withStatus status: Alert) {
+        DispatchQueue.main.async { [unowned self] in
+            let alert = UIAlertController(
+                title: status.title,
+                message: status.message,
+                preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default)
+            alert.addAction(okAction)
+            present(alert, animated: true)
+        }
+    }
 }
 
